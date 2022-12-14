ac8354756367de1bb3a57c7e204e157c6bd4b48c
avrora.io <!B1mujx22>
2022-12-14 06:05:04 -0800

    .

--- src/classes/mongo.ts
+++ src/classes/mongo.ts
@@ -1,32 +1,42 @@
 type DatabaseName = string
 type ColumnName = string
+type ObjectId = any
+
+export interface InsertReturn {
+    // insertOne
+    acknowledged   : boolean,
+    insertedId    ?: ObjectId
+    // insert - insertMany
+    insertedCount ?: number,
+    insertedIds   ?: { [key:string]: ObjectId }
+}
+
 export interface DBSchema {
     [key:DatabaseName] : {
         [key:ColumnName] : {
-            insert(query)
+            insert(query)    : InsertReturn
+            insertOne(query) : InsertReturn
             find(query)
         }
     }
 }
 const { MongoClient } = require('mongodb');
 
-
 // Connection URL
 const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
 
-export class Mongo {
+export class Mongo extends MongoClient{
     static DBSchema:DBSchema
     #client
     readonly #config
     constructor(DBSchema) {
+        super(url)
         this.#config = {
             databases : DBSchema
         };
     }
     async connect<R extends string, schema extends DBSchema>(url:R): Promise <schema> {
-        let instance = new MongoClient(url, { useUnifiedTopology: true });
-        this.#client = await instance.connect();
+        this.#client = await super.connect();
         if(this.#config){
             let dbs = Object.keys(this.#config.databases);
             for(let i = 0; i < dbs.length; i++){
